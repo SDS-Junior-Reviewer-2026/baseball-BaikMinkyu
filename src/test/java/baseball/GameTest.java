a7327bc24b8b6fd2bf9f0bfa1fac58796adc6bb9
0713jake <0713jake@naver.com>
2026-04-03 14:07:23 +0900

    [refactoring] 68p

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -30,32 +30,16 @@ public class GameTest {
 
 	@Test
 	public void 숫자_세개가_전부_일치_할_경우_3_strike() {
-		game.question = "123";
-		GuessResult result = game.guess("123");
+		generateQuestion("123");
 
-		Assertions.assertThat(result)
-				.isNotNull();
-		Assertions.assertThat(result.getIsSolved())
-				.isTrue();
-		Assertions.assertThat(result.getStrikes())
-				.isEqualTo(3);
-		Assertions.assertThat(result.getBalls())
-				.isEqualTo(0);
+		assertThatGuessResultIsEqual(game.guess("123"), true, 3, 0);
 	}
 
 	@Test
 	public void 숫자_세개가_전부_일치_하지_않을_경우_0_strike_0_ball() {
-		game.question = "123";
-		GuessResult result = game.guess("456");
+		generateQuestion("123");
 
-		Assertions.assertThat(result)
-				.isNotNull();
-		Assertions.assertThat(result.getIsSolved())
-				.isFalse();
-		Assertions.assertThat(result.getStrikes())
-				.isEqualTo(0);
-		Assertions.assertThat(result.getBalls())
-				.isEqualTo(0);
+		assertThatGuessResultIsEqual(game.guess("456"), false, 0, 0);
 	}
 
 	@Test
@@ -73,9 +57,24 @@ public class GameTest {
 
 	}
 
+	private void generateQuestion(String question) {
+		game.question = question;
+	}
+
 	private void assertThrowsIllegalArgument(String guessNumber) {
 		assertThrows(IllegalArgumentException.class, () -> {
 			game.guess(guessNumber);
 		});
+	}
+
+	private void assertThatGuessResultIsEqual(GuessResult result, boolean isSolved, int strikes, int balls) {
+		Assertions.assertThat(result)
+				.isNotNull();
+		Assertions.assertThat(result.getIsSolved())
+				.isEqualTo(isSolved);
+		Assertions.assertThat(result.getStrikes())
+				.isEqualTo(strikes);
+		Assertions.assertThat(result.getBalls())
+				.isEqualTo(balls);
 	}
 }
